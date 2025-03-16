c5eb05c7c65f28f8c1033bc81496d0b949847cde
Xiomara1534 <cp0460032023@unab.edu.sv>
2025-03-16 01:01:38 -0600

    Creación de las interfaces

---
+++ src/main/java/com/mycompany/interfacesproyecto/InterfacesProyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfacesproyecto;
+
+/**
+ *
+ * @author xioma
+ */
+public class InterfacesProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
